163fdb33d644decdae212c07885b5123693a39f4
Ussura-Cranell <116423358+Ussura-Cranell@users.noreply.github.com>
2024-04-17 00:50:38 +0300

    A few tests to check

--- src/main/java/com/mygame/GuiDemo.java
+++ src/main/java/com/mygame/GuiDemo.java
@@ -1,6 +1,11 @@
 package com.mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.asset.plugins.FileLocator;
+import com.jme3.material.Material;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.texture.Texture;
 import com.simsilica.lemur.Button;
 import com.simsilica.lemur.Command;
 import com.simsilica.lemur.Container;
@@ -40,11 +45,39 @@ public class GuiDemo extends SimpleApplication {
         myWindow.addChild(new Label("Hello, World."));
         Button clickMe = myWindow.addChild(new Button("Click Me"));
         clickMe.addClickCommands(new Command<Button>() {
+            static int clickCount = 0;
             @Override
             public void execute( Button source ) {
-                System.out.println("The world is yours.");
-
+                System.out.println("Number of clicks: " + ++clickCount);
             }
         });
+
+        // Other code
+        assetManager.registerLocator("src/main/java/com/assets", FileLocator.class);
+
+        float Offset = 5.0f;
+        createBox(0.0f, 0.0f, 0.0f);
+        createBox(Offset, 0.0f, 0.0f);
+        createBox(0.0f, Offset, 0.0f);
+        createBox(0.0f, 0.0f, Offset);
+    }
+
+    private void createBox(float x, float y, float z, float scaleX, float scaleY, float scaleZ, Material boxMaterial){
+        Box boxShape = new Box(scaleX, scaleY, scaleZ);
+        Geometry boxGeometry = new Geometry("box", boxShape);
+        boxGeometry.move(x, y, z);
+
+        if (boxMaterial==null){
+            boxMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+            //boxMaterial.setColor("Color", ColorRGBA.Yellow);
+            Texture boxTestTexture = assetManager.loadTexture("Textures/test-texture.jpg");
+            boxMaterial.setTexture("ColorMap", boxTestTexture);
+            boxGeometry.setMaterial(boxMaterial);
+        }
+        rootNode.attachChild(boxGeometry);
+    }
+
+    private void createBox(float x, float y, float z){
+        this.createBox(x, y, z, 1.0f, 1.0f, 1.0f, null);
     }
 }
